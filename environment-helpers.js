b81056f0d71775e5be957fb1267e9d34e02e1285
David Cowan <dacowan@microsoft.com>
2022-08-18 15:23:13 -0700

    Added support for Extension Resource Packs
    - `yarn prepare` will now prepare both the sample behavior pack AND a sample resource pack (if it's specified in the `.env` file)
    - Added sample localization string
    - Added sample icon for using with the sample extension
    - Added optional dependencies to the behavior pack manifest to refer to the resource pack

--- environment-helpers.js
+++ environment-helpers.js
@@ -7,9 +7,9 @@ import path from "path";
 require("dotenv").config();
 
 export function expandVariablesWithEnvironmentVariables(inputString) {
-  // find/replace all expandable variables '{var-name}' and replace them
+  // find/replace all expandable variables '{VAR_NAME}' and replace them
   // with the ones defined in the .env file
-  const variableRegExp = /{(.+)}/g;
+  const variableRegExp = /{([A-Z|_|0-9]+)}/g;
   var match = variableRegExp.exec(inputString);
   if (!match) {
     throw "The input string did not have any expansion variables";
@@ -36,6 +36,21 @@ export function expandVariablesWithEnvironmentVariables(inputString) {
   return inputString;
 }
 
+export function shouldGenerateResourcePack() {
+  var requirement = process.env.MC_EXTENSION_GENERATE_RESOURCE_PACK;
+  if(!requirement) {
+    return false;
+  }
+  requirement = requirement.toLowerCase();
+  if(requirement === 'yes' || requirement === '1' || requirement === 'true') {
+    return true;
+  }
+  else if( requirement === 'no' || requirement === '0' || requirement === 'false') {
+    return false;
+  }
+  throw 'MC_EXTENSION_GENERATE_RESOURCE_PACK in .env is set to an invalid value [' + requirement + ']';
+}
+
 export function getExtensionName() {
   const extensionName = process.env.MC_EXTENSION_NAME;
   if (!extensionName) {
@@ -45,23 +60,47 @@ export function getExtensionName() {
 }
 
 export function getBehaviorPackName() {
-  const packName = process.env.MC_BEHAVIOR_PACK_NAME;
+  const packName = process.env.MC_PACK_NAME;
   if (!packName) {
     throw "Unable to resolve Behavior Pack Name from .env file";
   }
   return packName;
 }
 
+export function getResourcePackName() {
+  const packName = process.env.MC_PACK_NAME;
+  if (!packName) {
+    throw "Unable to resolve Resource Pack Name from .env file";
+  }
+  return packName;
+}
+
 export function getBehaviorPackFolder(exePrefix) {
   const behaviorPackName = getBehaviorPackName();
-  const outputDir = path.resolve(getTargetFolder(exePrefix), behaviorPackName);
+  const rootFolder = getTargetFolder(exePrefix);
+  const outputDir = path.resolve(rootFolder, 'development_behavior_packs', behaviorPackName);
   return outputDir;
+}
+
+export function getResourcePackFolder(exePrefix) {
+  const resourcePackName = getResourcePackName();
+  const rootFolder = getTargetFolder(exePrefix);
+  const outputDir = path.resolve(rootFolder, 'development_resource_packs', resourcePackName);
+  return outputDir;
+}
+
+export function getResourcePackUUID() {
+  const uuid = process.env.MC_RESOURCE_PACK_UUID;
+  if(!uuid) {
+    throw 'Unable to resolve resource pack uuid from .env file';
+  }
+  return uuid;
 }
 
 export function getTargetFolder(exePrefix) {
   var outputDir = process.env["MC_PACK_ROOT_" + exePrefix];
   if (!outputDir) {
-    throw "Unable to resolve " + exePrefix + " output path from .env file";
+    throw "Unable to resolve MC_PACK_ROOT_" + exePrefix + " output path from .env file";
   }
 
   // Expand any environment variables
--- just.config.ts
+++ just.config.ts
@@ -19,22 +19,15 @@ task('bundle', () => {
 });
 
 task('prepare', () => {
-	const exeType: string = (argv().exetype as string).toLowerCase();
+	const exeType: string = (argv().exetype as string).toUpperCase();
 	if(!exeType) {
 		throw 'No exetype option specified - unknown target platform';
 	}
 
 	const forcePrepare: boolean = (argv().forceprepare ? argv().forceprepare as boolean : false);
 
-	if(exeType === 'win32') {
-		_prepareTargetFolder('Win32', forcePrepare);
-	}
-	else if( exeType === 'uwp') {
-		_prepareTargetFolder('UWP', forcePrepare);
-	}
-	else {
-		throw 'Unrecognized target platform';
-	}
+	_prepareBehaviorPackFolder(exeType, forcePrepare);
+	_prepareResourcePackFolder(exeType, forcePrepare);
 });
 
 task('copyBuildToTarget', () => {
@@ -98,11 +91,159 @@ task('clean', () => {
 });
 
 /*
-  Internal function to create and prepare the destination folder,
+  Internal function to create and prepare the resource pack folder,
   and modify the assets contained within to match with the settings
   stored in the .env file
 */
-function _prepareTargetFolder(exeType: string, force: boolean) {
+function _prepareResourcePackFolder(exeType: string, force: boolean) {
+	if( !envHelpers.shouldGenerateResourcePack()) {
+		logger.info('Skipping Resource Pack generation');
+		return false;
+	}
+
+	logger.info('Preparing destination resource pack folder (' + exeType + ')');
+
+	const extensionName = envHelpers.getExtensionName();
+	const targetFolder = envHelpers.getResourcePackFolder(exeType);
+	if(!targetFolder) {
+		throw 'Unable to resolve target resource pack folder';
+	}
+
+	// Check that the resource pack folder exists
+	try {
+		if (!fs.existsSync(targetFolder)) {
+			logger.info('Creating resource pack folder [' + targetFolder + ']');
+			fs.mkdirSync(targetFolder);
+		}
+		else {
+			// Resource Pack folder exists - no need to carry on unless we're
+			// forced to
+			if( !force ) {
+				logger.info('Resource Pack exists - skipping prepare step');
+				return;
+			}
+			logger.info('Forcing Resource Pack prepare step');
+		}
+	} catch(err) {
+		logger.error(err);
+		throw err;
+	}
+
+	// Copy our assets into that folder
+	const sourcePath = path.resolve(__dirname, 'assets', 'resource');
+	logger.info('Copying template files to destination (' + targetFolder + ')');
+	try{
+		// check that our assets folder exists!
+		if(!fs.existsSync(sourcePath)) {
+			const msg = 'Failed to locate template `resource assets` folder';
+			logger.error(msg);
+			throw msg;
+		}
+	} catch(err) {
+		logger.error(err);
+		throw err;
+	}
+
+	// Copy the folder contents to the destination
+	fs.copySync(sourcePath, targetFolder, {overwrite: true}, (err) => {
+		if(err) {
+			logger.error(err);
+			throw err;
+		}
+		logger.info('Copy Success');
+	});
+
+		// Now grep through the manifest file and expand upon the variables
+		logger.info('Preparing Manifest file');
+
+		var manifest: string;
+		const manifestFilename = path.resolve(targetFolder, 'manifest.json');
+		try {
+			// Load the manifest file
+			manifest = fs.readFileSync(manifestFilename, 'utf8');
+		} catch(err) {
+			logger.error(err);
+			throw err;
+		}
+	
+		// find/replace all expandable variables '{var-name}' and replace them
+		// with the ones defined in the .env file
+		manifest = envHelpers.expandVariablesWithEnvironmentVariables(manifest);
+	
+		// Now, replace the manifest file with the updated one
+		try {
+			// Write out the new file
+			fs.writeFileSync(manifestFilename, manifest, {encoding: 'utf8', flag: 'w'});
+		} catch(err) {
+			logger.error(err);
+			throw err;
+		}
+	
+	
+	// Rebuild the textures_list.json
+	const texturesPath = path.resolve(sourcePath, 'textures');
+	const textureList = _getFileList(texturesPath, ['png', 'tga', 'jpg', 'jpeg'], sourcePath);
+	
+	const textureListFilename = path.resolve(targetFolder, 'textures', 'textures_list.json');
+
+	// Now, write out the new textures_list.json file
+	try {
+		// Write out the new file
+		fs.writeFileSync(textureListFilename, JSON.stringify(textureList), {encoding: 'utf8', flag: 'w'});
+	} catch(err) {
+		logger.error(err);
+		throw err;
+	}
+
+	// We need to add the dependency for the resource pack to the behavior pack
+	const behaviorPackFolder = envHelpers.getBehaviorPackFolder(exeType);
+	const behaviorPackManifestFilename = path.resolve(behaviorPackFolder, 'manifest.json');
+	const resourcePackUUID = envHelpers.getResourcePackUUID();
+	var behaviorPackManifest = require(behaviorPackManifestFilename);
+
+	interface dependency {
+		module_name?: string;
+		uuid: string;
+		version: number[];
+	};
+
+	// Let's make sure we haven't added it already
+	var found = false;
+	for(const d of behaviorPackManifest.dependencies) {
+		const dep: dependency = d as dependency;
+		if(dep && dep.uuid === resourcePackUUID) {
+			found = true;
+			break;
+		}
+	}
+
+	if(!found) {
+		logger.info('Adding resource pack dependency to behavior pack');
+
+		// Insert the new dependency object into the JSON object
+		const newKey: dependency = {uuid: envHelpers.getResourcePackUUID(), version: [0,0,1]};
+		behaviorPackManifest.dependencies.push( newKey );
+
+		// Now re-write the file 
+		try {
+			// Write out the modified file
+			fs.writeFileSync(behaviorPackManifestFilename, JSON.stringify(behaviorPackManifest), {encoding: 'utf8', flag: 'w'});
+		} catch(err) {
+			logger.error(err);
+			throw err;
+		}
+	}
+	else {
+		logger.info('Skipping dependency addition to behavior pack - already found');
+	}
+
+}
+/*
+  Internal function to create and prepare the behavior pack folder,
+  and modify the assets contained within to match with the settings
+  stored in the .env file
+*/
+function _prepareBehaviorPackFolder(exeType: string, force: boolean) {
 
 	logger.info('Preparing destination behavior pack folder (' + exeType + ')');
 
@@ -133,12 +274,12 @@ function _prepareTargetFolder(exeType: string, force: boolean) {
 	}
 
 	// Copy our assets into that folder
-	const sourcePath = path.resolve(__dirname, 'assets');
+	const sourcePath = path.resolve(__dirname, 'assets', 'behavior');
 	logger.info('Copying template files to destination (' + targetFolder + ')');
 	try{
 		// check that our assets folder exists!
 		if(!fs.existsSync(sourcePath)) {
-			const msg = 'Failed to locate template `assets` folder';
+			const msg = 'Failed to locate template `behavior assets` folder';
 			logger.error(msg);
 			throw msg;
 		}
@@ -182,3 +323,25 @@ function _prepareTargetFolder(exeType: string, force: boolean) {
 		throw err;
 	}
 }
+
+function _getFileList(dirName: string, extensions: string[], pathPrefix: string) {
+    var files: string[] = [];
+
+    const items = fs.readdirSync(dirName, { withFileTypes: true });
+    for (const item of items) {
+        if (item.isDirectory()) {
+            files = [...files, ..._getFileList(`${dirName}/${item.name}`,extensions, pathPrefix)];
+        } else {
+			for(const extension of extensions) {
+				if(item.name.endsWith(extension)) {
+					const fullName = `${dirName}/${item.name}`;
+					const relativeName = fullName.substring(pathPrefix.length+1);
+					files.push(relativeName);
+					break;
+				}
+			}
+        }
+    }
+
+    return files;
+};
